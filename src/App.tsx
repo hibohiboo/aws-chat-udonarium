66597aab3a6e49f8557f2cdc20648c2b4e2f3156
hibohiboo <swdice66@gmail.com>
2022-12-19 08:52:44 +0900

    fix 部屋一覧 UI AB#261

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,19 @@ import useUdonariumChat from './hooks/useUdonariumChat';
 
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 
+const rowStyle = {
+  roomId: { display: 'inline-block', width: '50px' },
+  roomName: {
+    display: 'inline-block',
+    width: '150px',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    verticalAlign: 'top', // overflow: hiddenだとvertical-align:baseline が要素ボックスの垂直位置を参照してしまうためずれる
+  },
+  hasPassword: { display: 'inline-block', width: '30px' },
+  numberOfEntrants: { display: 'inline-block', width: '30px' },
+} as const;
 const App: React.FC = () => {
   const { rooms } = useUdonariumChat();
 
@@ -13,11 +26,18 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <ul style={{ listStyle: 'none', width: '300px' }}>
+        <li style={{ textAlign: 'center', fontWeight: 'bold' }}>
+          <span style={rowStyle.roomId}>ID</span>
+          <span style={rowStyle.roomName}>部屋名</span>
+          <span style={rowStyle.hasPassword}>🔒️</span>
+          <span style={rowStyle.numberOfEntrants}>👥</span>
+        </li>
         {rooms.map((room) => (
-          <li key={room.alias}>
-            <span>{room.name}</span>
-            <span>{`${room.hasPassword}`}</span>
-            <span>{room.numberOfEntrants}</span>
+          <li key={room.alias} style={{ textAlign: 'center' }}>
+            <span style={rowStyle.roomId}>{room.id}</span>
+            <span style={rowStyle.roomName}>{room.name}</span>
+            <span style={rowStyle.hasPassword}>{`${room.hasPassword ? '🔒️' : ''}`}</span>
+            <span style={rowStyle.numberOfEntrants}>{room.numberOfEntrants}</span>
           </li>
         ))}
       </ul>
