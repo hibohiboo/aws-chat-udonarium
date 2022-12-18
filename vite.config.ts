db8aae064cb6af3a364c50c0000519ab8ac5c91e
hibohiboo <swdice66@gmail.com>
2022-12-18 16:29:07 +0900

    chore update base path AB#260

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// @ts-ignore
+const dev = process.env.npm_lifecycle_event === 'dev';
+const base = dev ? '' : `/mobile-udonarium/`
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base,
 })
